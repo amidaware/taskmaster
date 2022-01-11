6f66151b9be0ff6dc341b0c4d4824cbf82627b93
wh1te909 <dcparsi@gmail.com>
2022-01-10 17:18:36 -0800

    Revert "test"
    This reverts commit 9c4ae652beadab23689640722f4282b6311d1b6d.

--- validate.go
+++ validate.go
@@ -1,4 +1,3 @@
-//go:build windows
 // +build windows
 
 package taskmaster
@@ -91,6 +90,8 @@ func validateTriggers(triggers []Trigger) error {
 				return errors.New("invalid MonthlyTrigger: StartBoundary is required")
 			} else if t.DaysOfMonth == 0 {
 				return errors.New("invalid MonthlyTrigger: DaysOfMonth is required")
+			} else if t.DaysOfMonth > AllDaysOfMonth  {
+				return errors.New("invalid MonthlyTrigger: invalid DaysOfMonth")
 			} else if t.MonthsOfYear == 0 {
 				return errors.New("invalid MonthlyTrigger: MonthsOfYear is required")
 			} else if t.MonthsOfYear > AllMonths {
